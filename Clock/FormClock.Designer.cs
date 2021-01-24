07b215ad1af6dd7be428718ce16d025d9cd32a1d
Andrej Pekar <pekand@gmail.com>
2021-01-24 12:23:51 +0100

    init

---
+++ Clock/FormClock.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Clock
+{
+    partial class FormClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormClock));
+            this.labelClock = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelDay = new System.Windows.Forms.Label();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClock.Location = new System.Drawing.Point(12, 9);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(417, 108);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "00:00:00";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(24, 130);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(163, 33);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "2021-01-01";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDay.Location = new System.Drawing.Point(311, 130);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(118, 33);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "Monday";
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alwaysOnTopToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(150, 48);
+            this.contextMenu.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenu_Opening);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Location = new System.Drawing.Point(30, 192);
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 5;
+            // 
+            // FormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 168);
+            this.ContextMenuStrip = this.contextMenu;
+            this.Controls.Add(this.monthCalendar);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelClock);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "00:00:00";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FormClock_Load);
+            this.Resize += new System.EventHandler(this.FormClock_Resize);
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+    }
+}
+
---
+++ Clock/FormClock.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class FormClock : Form
+    {
+        public FormClock()
+        {
+            InitializeComponent();
+        }
+
+        private void FormClock_Load(object sender, EventArgs e)
+        {
+            this.updateClock();
+            this.resizeElements();
+            this.timer.Enabled = true;
+
+            this.monthCalendar.SetDate(DateTime.Now);
+        }
+
+        public string getTime() {
+            DateTime localDate = DateTime.Now;
+
+            return 
+                localDate.Hour.ToString().PadLeft(2, '0') + ":"+
+                localDate.Minute.ToString().PadLeft(2, '0') + ":"+
+                localDate.Second.ToString().PadLeft(2, '0');
+
+        }
+
+        public string getDate()
+        {
+            DateTime localDate = DateTime.Now;
+
+            return
+                localDate.Year.ToString() + "-" +
+                localDate.Month.ToString().PadLeft(2, '0') + "-" +
+                localDate.Day.ToString().PadLeft(2, '0') + " " +
+                localDate.ToString("MMMM") + ", " + 
+                localDate.ToString("dddd");
+
+        }
+
+        public string getDay()
+        {
+            return "";
+
+        }
+
+        public void updateClock() {
+            string time = this.getTime();
+
+            this.Text = time;
+            labelClock.Text = time;
+
+            labelDate.Text = this.getDate();
+
+            labelDay.Text = this.getDay();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.updateClock();
+        }
+
+        private void contextMenu_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+        }
+
+
+        public void resizeElements() {
+            labelClock.Font = new Font(labelClock.Font.FontFamily, this.Width / 7);
+            labelDate.Font = new Font(labelDate.Font.FontFamily, this.Width / 20);
+            labelDay.Font = new Font(labelDay.Font.FontFamily, this.Width / 20);
+
+            labelDate.Top = labelClock.Top + labelClock.Height + 10;
+            labelDay.Top = labelClock.Top + labelClock.Height + 10;
+            labelDay.Left = labelClock.Left + labelClock.Width - labelDay.Width;
+
+            monthCalendar.Top = labelDate.Top + labelDate.Height + 10;
+        }
+
+        private void FormClock_Resize(object sender, EventArgs e)
+        {
+            this.resizeElements();
+        }
+
+        
+    }
+}
