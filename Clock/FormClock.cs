7093aec21c03feddfe7fb94742000640e60aec54
Andrej Pekar <pekand@gmail.com>
2021-01-27 10:49:27 +0100

    clock

--- Clock/FormClock.cs
+++ Clock/FormClock.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ namespace Clock
 {
     public partial class FormClock : Form
     {
+        DateTime stopWatch;
+        double stopWatchCount = 0;
+        bool stopWatchRunning = false;
+        bool stopWatchPaused = false;
+
         public FormClock()
         {
             InitializeComponent();
@@ -24,6 +30,28 @@ namespace Clock
             this.timer.Enabled = true;
 
             this.monthCalendar.SetDate(DateTime.Now);
+
+            this.Height = 200;
+            this.Width = 430;
+
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+
+            DaylightTime daylight =
+            localZone.GetDaylightChanges(DateTime.Now.Year);
+
+            
+            this.labelInfo.Text = 
+                "Timezone: "+localZone.StandardName + 
+                " \nDaylight saving time name: " + localZone.DaylightName + 
+                " \nDaylight saving time: " + localZone.IsDaylightSavingTime(DateTime.Now)+
+
+                " \nDaylight start: " + daylight.Start.ToString() +
+                " \nDaylight end: " + daylight.End.ToString() +
+                " \nDelta: " + daylight.Delta +
+
+                " \nUTC time: " + localZone.ToUniversalTime(DateTime.Now) +
+                " \nUTC offset: " + localZone.GetUtcOffset(DateTime.Now)
+                ;
         }
 
         public string getTime() {
@@ -58,17 +86,29 @@ namespace Clock
         public void updateClock() {
             string time = this.getTime();
 
-            this.Text = time;
-            labelClock.Text = time;
+            if (this.Text != time) {
+                this.Text = time;
+                labelClock.Text = time;
+                labelDate.Text = this.getDate();
+                labelDay.Text = this.getDay();
+            }
+        }
 
-            labelDate.Text = this.getDate();
-
-            labelDay.Text = this.getDay();
+        public void updateStopWatch()
+        {
+            if (this.stopWatchRunning) {
+                double diff = this.stopWatchCount + DateTime.Now.Subtract(this.stopWatch).TotalSeconds;
+                double minutest = (int)diff / 60;
+                double seconds = diff - (minutest * 60);
+                this.labelStopWatch.Text = minutest.ToString() + ":" + seconds.ToString();
+            }
+            
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             this.updateClock();
+            this.updateStopWatch();
         }
 
         private void contextMenu_Opening(object sender, CancelEventArgs e)
@@ -96,7 +136,7 @@ namespace Clock
             labelDay.Top = labelClock.Top + labelClock.Height + 10;
             labelDay.Left = labelClock.Left + labelClock.Width - labelDay.Width;
 
-            monthCalendar.Top = labelDate.Top + labelDate.Height + 10;
+            panelTools.Top = labelDate.Top + labelDate.Height + 10;
         }
 
         private void FormClock_Resize(object sender, EventArgs e)
@@ -104,6 +144,44 @@ namespace Clock
             this.resizeElements();
         }
 
-        
+        private void buttonStartStopWatch_Click(object sender, EventArgs e)
+        {
+            if (!this.stopWatchRunning) {
+                this.stopWatch = DateTime.Now;
+                this.stopWatchRunning = true;
+            }
+
+            if (this.stopWatchRunning)
+            {
+                
+            }
+
+            if (this.stopWatchPaused)
+            {
+
+            }
+        }
+
+        private void buttonPauseStopWatch_Click(object sender, EventArgs e)
+        {
+            if (this.stopWatchRunning) {
+                this.stopWatchRunning = false;
+                this.stopWatchPaused = true;
+                this.stopWatchCount += DateTime.Now.Subtract(this.stopWatch).TotalSeconds;
+            }
+        }
+
+        private void buttonResertStopWatch_Click(object sender, EventArgs e)
+        {
+            if (this.stopWatchRunning || this.stopWatchPaused)
+            {
+                this.stopWatchRunning = false;
+                this.stopWatchPaused = false;
+                this.stopWatchCount = 0;
+                this.stopWatch = DateTime.Now;
+                this.labelStopWatch.Text = "0";
+            }
+            
+        }
     }
 }
